a14376394d752eab28c063a1823653711a9fd588
Joshua Downer <joshua.downer@gmail.com>
2016-05-29 07:20:28 -0400

    update example

--- ex.asm
+++ ex.asm
@@ -1,17 +1,22 @@
-LDA 9
-STA 50
-NUL
-LDA 65
-STA 0
-LDA 108
-STA 0
-LDA 101
-STA 0
-LDA 120
-STA 0
-LDA 10
-STA 0
-INC 51
-CMP 50 51
-BRA 36
-HLT
+start:
+  LDA 9
+  STA iteration + 1
+iteration:
+  NUL
+print:
+  CMP iteration + 1, iteration
+  BRB done
+  LDA 65
+  STA 0
+  LDA 108
+  STA 0
+  LDA 101
+  STA 0
+  LDA 120
+  STA 0
+  LDA 10
+  STA 0
+  INC iteration
+  JMP print
+done:
+  HLT
